58996ba77006c4594fd8c6daeb667bc22f7e6d4d
daniel phelipe <danielphelipe12@hotmail.com>
2026-09-09 19:54:28 -0300

    Update alpha.py

--- alpha.py
+++ alpha.py
@@ -288,7 +288,7 @@ tipo_usuario = user.get("tipo", "vendedor")
 
 with st.sidebar:
     st.markdown(f"### 👤 Logado como:\n**{user.get('nome', '')}**")
-    st.caption(f"Perfil: {user['tipo'].upper()}")
+    st.caption(f"Perfil: {tipo_usuario.upper()}")
     
     if st.button("Sair (Logout)", use_container_width=True):
         logout()
