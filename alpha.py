95ac3c7cd5afab2b903534858e1c953ba3ab1e51
daniel phelipe <danielphelipe12@hotmail.com>
2026-09-09 20:32:29 -0300

    Update alpha.py

--- alpha.py
+++ alpha.py
@@ -2,10 +2,9 @@ import streamlit as st
 import pandas as pd
 from sqlalchemy import create_engine, text
 from urllib.parse import quote_plus
-
 import os
+import sys
 
-# Adiciona a pasta atual ao path do Python para garantir que ele encontre a pasta views
 current_dir = os.path.dirname(os.path.abspath(__file__))
 if current_dir not in sys.path:
     sys.path.append(current_dir)
