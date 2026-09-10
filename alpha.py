e4a13b0dbb655517fa2c63c71802d83882dc226e
daniel phelipe <116456867+danielph1@users.noreply.github.com>
2026-09-10 10:20:50 -0300

    Update alpha.py

--- alpha.py
+++ alpha.py
@@ -303,8 +303,7 @@ with st.sidebar:
 
 # --- PAGINA DE VENDEDORES ---
     btn_p_vendedores = "primary" if st.session_state["pagina_atual"] == "vendedores" else "secondary"
-    if st.button("Equipe de Vendedores", use_container_width=True, type=btn_p_vendedores,
-                 disable = not user["vendedor"]):
+    if st.button("Equipe de Vendedores", use_container_width=True, type=btn_p_vendedores):
         st.session_state["pagina_atual"] = "vendedores"
         st.rerun()
 
